641e42764196495b61aa7adf1410179a62933db2
Dr.K.Kalaivani <k.kalaivani@sru.edu.in>
2025-10-19 01:24:33 +0530

    Create CalculatorTest.java

---
+++ src/main/java/src/test/java/CalculatorTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+    @Test
+    public void testAdd() {
+        assertEquals(5, Calculator.add(2, 3));
+    }
+}
